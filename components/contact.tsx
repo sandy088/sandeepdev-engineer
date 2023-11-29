acd35daa9fb4e9cdedf87c6a3843044982e426df
Sandeep <sandeeppakho55@gmail.com>
2023-11-29 08:45:04 +0530

    Fix: Minor

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
+        <a className="underline" href="mailto:sandeep.personal03@gmail.com">
           sandeep.personal03@gmail.com
         </a>{" "}
         or through this form.
